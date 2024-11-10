02f46305001fe3e309bd9318083295c4a8850703
Chris <c.urffer@live.com>
2024-11-10 16:47:06 -0500

    WIP: adding recipe view page

--- app.py
+++ app.py
@@ -7,25 +7,50 @@ db.create_tables([Recipe, Ingredient, RecipeToIngredient])
 
 app = Flask(__name__)
 
+current_recipe = None
+
 @app.route("/")
 def route_main_page():
     return (
-        f"<p>Hello, World!</p>" +
         f"<div>" +
         f"<h2>Recipes</h2>" +
         "".join([
-            f"<div>{recipe.name}</div>"
+            f"<div><a  href='recipe_view/{recipe.name}'>{recipe.name}</a></div>"
             for recipe in Recipe.select()
         ]) +
         f"</div>" +
-        f"<p>Add New Recipe:</p>" +
+        f"<h2>Add New Recipe:</h2>" +
         f"<form method='post' action='/recipe_add'>" +
-        f"<input type='text' name='name' placeholder='Name'>" +
-        f"<input type='text' name='description' placeholder='Description'>" +
-        f"<input type='text' name='ingredients' placeholder='Ingredients'>" +
-        f"<input type='text' name='steps' placeholder='Steps'>" +
-        f"<input type='submit'>" +
+        f"<div><input type='text' name='name' placeholder='Name'></div>" +
+        f"<div><input type='text' name='description' placeholder='Description'></div>" +
+        f"<div><input type='text' name='ingredients' placeholder='Ingredients'></div>" +
+        f"<div><input type='text' name='steps' placeholder='Steps'></div>" +
+        f"<div><input type='submit'></div>" +
         f"</form>"
+    )
+
+@app.route("/recipe_view/<name>")
+def route_view_recipe(name: str):
+    recipe: Recipe = Recipe.get(Recipe.name == name)
+
+    def render_ingredient_list(recipe: Recipe, indentation: str = ""):
+        return (
+            f"<div>" +
+            "".join([
+                f"<div>{indentation}{ingredient.name}</div>"
+                for ingredient in recipe.ingredients
+            ]) +
+            f"</div>"
+        )
+
+    return (
+        f"<div>" +
+        f"<h2>{recipe.name}</h2>" +
+        f"<h3>Description:</h3>" +
+        f"<p>{recipe.description}</p>" +
+        f"<h3>Ingredients:</h3>" +
+        render_ingredient_list(recipe) +
+        f"</div>"
     )
 
 @app.route("/recipe_add")
